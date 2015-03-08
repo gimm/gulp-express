87a6bab0eb4da516353f3923a7056a75e3815062
Gimm Yu <yucc2008@gmail.com>
2015-03-08 14:03:48 +0800

    re-organized for clarity; resolved issue #35

--- README.md
+++ README.md
@@ -13,15 +13,21 @@
 [8]: https://github.com/vohof/gulp-livereload/releases
 [9]: https://img.shields.io/github/tag/gimm/gulp-express.svg
 
-A gulp livereload plugin which does the following:
+A gulp plugin which serve the app with livereload, internally, it does the following:
  * use [`ChildProcess.spawn`](http://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options) to start a node process;
  * use [`tiny-lr`](https://github.com/mklabs/tiny-lr) provide livereload ability;
 
+## Install
+[![NPM](https://nodei.co/npm/gulp-express.png?compact=true)](https://nodei.co/npm/gulp-express/)
+
 ## Update notice
+* v0.3.0
+
+    > change signature of `server.run`. the third param `livereload` is used to config tiny-lr server.
+
 * v0.2.0
 
     > get `console.log` back.
-
 
 * v0.1.12
 
@@ -33,30 +39,29 @@ A gulp livereload plugin which does the following:
 * v0.1.5
     > pipe support added for [server.notify](#servernotifyevent)
 
-## Install
-```bash
-$ npm install --save-dev gulp-express
-```
 
 ## API
 
-### server.run([args][,options])
-Run or re-run the script file, which will create a server.
-Use the same arguments with [ChildProcess.spawn](http://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options) with 'node' as command.
+### server.run([args][,options][,livereload])
+Run/re-run the script file, which will create a http(s) server.
 
+Start a livereload(tiny-lr) server if it's not started yet.
+
+Use the same arguments with [ChildProcess.spawn](http://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options) with 'node' as command.
 
 * `args` - `Array` - Array List of string arguments. The default value is `['app.js']`.
 * `options` - `Object` - The third parameter for [ChildProcess.spawn](http://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options), the default value is:
 ```js
 options = {
-    cwd: undefined,
-    lr: {//lr will be used for tiny-lr server construction
-        port: 35729
-    }
+    cwd: undefined
 }
 options.env = process.env;
 options.env.NODE_ENV = 'development';
 ```
+* `livereload` - `Boolean|Number|Object` - The option for tiny-lr server. The default value is `35729`.
+    * `false` - will disable tiny-lr livereload server.
+    * `number` - treated as port number of livereload server.
+    *  `object` - used to create tiny-lr server `new tinylr.Server(livereload);`.
 * Returns a [ChildProcess](http://nodejs.org/api/child_process.html#child_process_class_childprocess) instance of spawned server.
 
 ### server.stop()
--- index.js
+++ index.js
@@ -4,102 +4,133 @@
 
 var util = require('util'),
     path = require('path'),
-    child_process = require('child_process'),
+    spawn = require('child_process').spawn,
     merge = require('deepmerge'),
     tinylr = require('tiny-lr'),
     es = require('event-stream'),
-    debug = require('debug')('gulp:express');
+    chalk = require('chalk'),
+    debug = require('debug')('gulp-express');
 
 module.exports = (function () {
-    var node = null,
-        defaults = {
-            args: ['app.js'],
-            options: {
-                cwd: undefined,
-                lr: {
-                    port: 35729
-                }
+    var server = undefined, // the server child process
+        lr = undefined, // tiny-lr server
+        config = {
+            args: ['app.js'],   // args for child_process.spawn
+            options: {  // options for child_process.options
+                cwd: undefined
+            },
+            livereload: {   //option for tiny-lr server
+                port: 35729
             }
-        };
-    defaults.options.env = process.env;
-    defaults.options.env.NODE_ENV = 'development';
-
-    var lr;
-    var livereload = {
-        start: function (livereloadOptions) {
-            if (lr == undefined) {
-                lr = tinylr(livereloadOptions);
-            }
-            lr.listen(livereloadOptions.port);
         },
-        reload: function (filename) {
-            if (lr != undefined) {
-                lr.changed({
-                    body: {
-                        files: [filename]
-                    }
-                });
-            } else {
-                debug('tinylr not started');
-                node && node.kill();
-            }
-        }
-    };
+        info = chalk.gray,
+        error = chalk.bold.red;
+    config.options.env = process.env;
+    config.options.env.server_ENV = 'development';
 
-    var listener = {
+    var callback = {
         processExit: function (code, sig) {
-            debug('Main process exited with [code => %s | sig => %s]', code, sig);
-            node && node.kill();
+            debug(info('Main process exited with [code => %s | sig => %s]'), code, sig);
+            server && server.kill();
         },
 
-        nodeExit: function (code, sig) {
-            debug('Node process exited with [code => %s | sig => %s]', code, sig);
-            if (lr != undefined) {
-                lr.close();
+        serverExit: function (code, sig) {
+            debug(info('server process exited with [code => %s | sig => %s]'), code, sig);
+            if(sig !== 'SIGKILL'){
+                process.exit(0);
             }
         },
 
-        logData: function (data) {
-            console.log(data.trim());
+        lrServerReady: function () {
+            console.log(info('livereload[tiny-lr] listening on %s ...'), config.livereload.port);
+        },
+
+        serverLog: function (data) {
+            console.log(info(data.trim()));
+        },
+
+        serverError: function (data) {
+            console.log(error(data.trim()));
         }
     };
 
     return {
-        run: function (args, options) {
-            args = (util.isArray(args) && args.length) ? args : defaults.args;
-            options = merge(defaults.options, options || {});
+        /**
+         * Start/restart a child process by running the script file,
+         * this process work as the http(s) server;
+         * And start a livereload(tiny-lr) server if it's not started yet.
+         *
+         * @param {array}                   [args]
+         * @param {object}                  [options]
+         * @param {boolean|number|object}   [livereload]
+         */
+        run: function (args, options, livereload) {
+            //deal with args
+            if(util.isArray(args) && args.length){
+                config.args = args;
+            }
 
-            if (node) { // Stop
-                node.kill('SIGKILL');
-                node = undefined;
-                process.removeListener('exit', listener.processExit);
+            //deal with options
+            config.options = merge(config.options, options || {});
+
+            //deal with livereload
+            if(livereload === false){   //livereload disabled
+                config.livereload = false;
+            }else if(livereload){
+                if(typeof livereload === 'object'){
+                    config.livereload = livereload;
+                }else{
+                    config.livereload.port = livereload;
+                }
+            }
+
+            if (server) { // server already running
+                server.kill('SIGKILL');
+                server = undefined;
             } else {
-                livereload.start(options.lr);
+                if(config.livereload){
+                    lr = tinylr(config.livereload);
+                    lr.listen(config.livereload.port, callback.lrServerReady);
+                }
             }
 
-            node = child_process.spawn('node', args, options);
-            node.stdout.setEncoding('utf8');
-            node.stderr.setEncoding('utf8');
-            node.stdout.on('data', listener.logData);
-            node.stderr.on('data', listener.logData);
-            node.on('exit', listener.nodeExit);
-            process.on('exit', listener.processExit);
+            server = spawn('node', config.args, config.options);
+            server.stdout.setEncoding('utf8');
+            server.stderr.setEncoding('utf8');
 
-            return node;
+            server.stdout.on('data', callback.serverLog);
+            server.stderr.on('data', callback.serverError);
+            server.on('exit', callback.serverExit);
+            process.on('exit', callback.processExit);
         },
+
+        /**
+         * Stop the server child process and the livereload server
+         */
         stop: function () {
-            if (node) {
-                node.kill('SIGKILL');
-                node = undefined;
+            if (server) {
+                //use SIGHUP instead of SIGKILL, see issue #34
+                server.kill('SIGKILL');
+                server = undefined;
+            }
+            if(lr){
+                lr.close();
+                lr = undefined;
             }
         },
+
+        /**
+         * Tell the livereload.js to reload the resources that been changed
+         * @param event
+         * @returns {*}
+         */
         notify: function (event) {
             if(event && event.path){
-                livereload.reload(path.relative(__dirname, event.path));
+                lr.changed({body: {files: [path.relative(__dirname, event.path)]}});
             }
 
             return es.map(function(file, done) {
-                livereload.reload(path.relative(__dirname, file.path));
+                lr.changed({body: {files: [path.relative(__dirname, file.path)]}});
                 done(null, file);
             });
         }
